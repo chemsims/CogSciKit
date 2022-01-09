335e389ab650ce08044017ae1a017138d5ff102a
Omar Fahmy <o.fahmy@outlook.com>
2022-01-09 15:05:19 +0400

    Added CGPoint extensions to move origin, reverse point and return point as size

--- Sources/CogSciKit/model/CoreGraphicsExtensions.swift
+++ Sources/CogSciKit/model/CoreGraphicsExtensions.swift
@@ -13,6 +13,33 @@ extension CGPoint {
     public func offset(_ size: CGSize) -> CGPoint {
         self.offset(dx: size.width, dy: size.height)
     }
+    
+    /// Returns the point as a size.
+    public var asSize: CGSize {
+        CGSize(width: x, height: y)
+    }
+    
+    /// Reverses the direction of the point.
+    public var reversed: CGPoint {
+        CGPoint(x: -x, y: -y)
+    }
+    
+    /// Returns a point in the same physical location, but measured from an origin
+    /// which has been moved by `offset`.
+    ///
+    ///     let point = CGPoint(x: 10, y: 10)
+    ///     let adjusted = point.moveOrigin(by: CGSize(width: 20, height: 20)
+    ///     print(adjusted)
+    ///     // prints CGPoint(x: -10, y: -10)
+    ///
+    /// Notice that `adjusted` refers to the same physical location, but only when measured from an
+    /// origin which is a distance `offset` away from the origin of the original point.
+    /// ```
+    public func moveOrigin(by offset: CGSize) -> CGPoint {
+        let dx = -offset.width
+        let dy = -offset.height
+        return self.offset(dx: dx, dy: dy)
+    }
 }
 
 extension CGSize {
--- Tests/CogSciKitTests/CoreGraphicsExtensionsTests.swift
+++ Tests/CogSciKitTests/CoreGraphicsExtensionsTests.swift
@@ -3,7 +3,7 @@
 //  
 
 import XCTest
-@testable import CogSciKit
+import CogSciKit
 
 class CoreGraphicsExtensionsTests: XCTestCase {
     
@@ -19,4 +19,40 @@ class CoreGraphicsExtensionsTests: XCTestCase {
         XCTAssertEqual(CGSize(width: -30, height: 100).scaled(by: 0), CGSize(width: 0, height: 0))
     }
     
+    func testReversingAPoint() {
+        XCTAssertEqual(CGPoint(x: 0, y: 0).reversed, CGPoint(x: 0, y: 0))
+        XCTAssertEqual(CGPoint(x: 20, y: 40).reversed, CGPoint(x: -20, y: -40))
+        XCTAssertEqual(CGPoint(x: -20, y: -40).reversed, CGPoint(x: 20, y: 40))
+    }
+    
+    func testMappingAPointToASize() {
+        XCTAssertEqual(CGPoint(x: 0, y: 0).asSize, CGSize(width: 0, height: 0))
+        XCTAssertEqual(CGPoint(x: 20, y: 40).asSize, CGSize(width: 20, height: 40))
+        XCTAssertEqual(CGPoint(x: -20, y: -40).asSize, CGSize(width: -20, height: -40))
+    }
+    
+    func testMovingAPointsOriginInTheXDirection() {
+        let point = CGPoint(x: 40, y: 0)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 0)).x, 40)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 10, height: 0)).x, 30)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 40, height: 0)).x, 0)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 100, height: 0)).x, -60)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: -10, height: 0)).x, 50)
+    }
+    
+    func testMovingAPointsOriginInTheYDirection() {
+        let point = CGPoint(x: 0, y: 50)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 0)).y, 50)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 20)).y, 30)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 50)).y, 0)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 60)).y, -10)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: -30)).y, 80)
+    }
+    
+    func testMovingAPointsOriginInBothDirections() {
+        let point = CGPoint(x:40, y: 50)
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 0, height: 0)), CGPoint(x: 40, y: 50))
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: 10, height: 20)), CGPoint(x: 30, y: 30))
+        XCTAssertEqual(point.moveOrigin(by: CGSize(width: -20, height: 40)), CGPoint(x: 60, y: 10))
+    }
 }
