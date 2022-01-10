1188fe73e92fdfe196bbeaedbc3ddb51c79a620a
Omar Fahmy <o.fahmy@outlook.com>
2022-01-10 19:06:37 +0400

    Moved SpeechBubble to CogSciKit

--- Sources/CogSciKit/styling/CorePalette.swift
+++ Sources/CogSciKit/styling/CorePalette.swift
@@ -8,4 +8,5 @@ public struct CorePalette {
     private init() { }
     
     public static let orangeAccent = Color("orangeAccent", bundle: .module)
+    public static let speechBubble = Color("speechBubble", bundle: .module)
 }
