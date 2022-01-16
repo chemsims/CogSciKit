78459078b1a9e49cbfc5c617465c4687a0415d41
Omar Fahmy <o.fahmy@outlook.com>
2022-01-16 17:47:25 +0400

    Moved functional extensions to CogSciKit

---
+++ Tests/CogSciKitTests/core-extensions/FunctionalExtensionsTests.swift
@@ -0,0 +1,19 @@
+//
+// CogSciKit
+//
+
+import XCTest
+import CogSciKit
+
+class FunctionalExtensionsTests: XCTestCase {
+
+    func testFlatteningArray() {
+        XCTAssertEqual([[1], [2]].flatten, [1, 2])
+        XCTAssertEqual([[1, 2], [3]].flatten, [1, 2, 3])
+        XCTAssertEqual([[1, 2], [3, 4, 5]].flatten, [1, 2, 3, 4, 5])
+        XCTAssertEqual([[1], [2], [], [3]].flatten, [1, 2, 3])
+        XCTAssertEqual([[], [2], [3,4,5], [6]].flatten, [2, 3, 4, 5, 6])
+        XCTAssert([[]].flatten.isEmpty)
+    }
+
+}
