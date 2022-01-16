0cedc1ef14cd6c3e739887cbc935813378c93caa
Omar Fahmy <o.fahmy@outlook.com>
2022-01-16 19:33:12 +0400

    Moved FixedText to CogSciKit

---
+++ Sources/CogSciKit/view/text/FixedText.swift
@@ -0,0 +1,24 @@
+//
+// CogSciKit
+//
+
+import SwiftUI
+
+public struct FixedText: View {
+
+    let value: String
+    public init(_ value: String) {
+        self.value = value
+    }
+
+    public var body: some View {
+        Text(value)
+            .fixedSize()
+    }
+}
+
+struct FixedText_Previews: PreviewProvider {
+    static var previews: some View {
+        FixedText("hello, world")
+    }
+}
