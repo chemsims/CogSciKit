67fdb08df063d61bc98558c4efc60318c406e2d0
Omar Fahmy <o.fahmy@outlook.com>
2022-02-01 20:54:30 +0400

    Fixed issue where chart lines were always displayed up to the discontinuity when one was provided

--- Sources/CogSciKit/view/chart/shapes/ChartLine.swift
+++ Sources/CogSciKit/view/chart/shapes/ChartLine.swift
@@ -44,7 +44,7 @@ public struct ChartLine: Shape {
     private var shiftedXAxis: LinearAxis<CGFloat> {
         xAxis.shift(by: offset)
     }
-
+    
     // TODO - make this safer by breaking early when the x range is 0
     public func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -70,36 +70,45 @@ public struct ChartLine: Shape {
         // We draw a small part of the line just before and after the discontinuity (if there is one)
         let discontinuityDelta = dx / 100
 
-        if let dc = discontinuity {
-            for x in strideLhs(dx: dx, discontinuity: dc) {
+        if let discontinuity = discontinuity, discontinuity <= endX {
+            let preDc = discontinuity - discontinuityDelta
+            for x in strideLhs(dx: dx, to: preDc) {
+                addLine(x: x, y: equation.getValue(at: x))
+            }
+            addLine(x: preDc, y: equation.getValue(at: preDc))
+            addLine(x: discontinuity, y: equation.getValue(at: discontinuity))
+            
+            let postDc = discontinuity + discontinuityDelta
+            
+            if postDc < endX {
+                addLine(x: postDc, y: equation.getValue(at: postDc))
+                for x in strideRhsOfDiscontinuity(dx: dx, from: postDc, to: endX) {
+                    addLine(x: x, y: equation.getValue(at: x))
+                }
+            }
+            addLine(x: endX, y: equation.getValue(at: endX))
+            
+        } else {
+            for x in strideAll(dx: dx) {
                 let y = equation.getValue(at: x)
                 addLine(x: x, y: y)
             }
-            let preDc = dc - discontinuityDelta
-            addLine(x: preDc, y: equation.getValue(at: preDc))
-            addLine(x: dc, y: equation.getValue(at: dc))
+            addLine(x: endX, y: equation.getValue(at: endX))
         }
-
-        for x in strideRhs(dx: dx, discontinuityDelta: discontinuityDelta) {
-            let y = equation.getValue(at: x)
-            addLine(x: x, y: y)
-        }
-        addLine(x: endX, y: equation.getValue(at: endX))
 
         return path
     }
-
-    private func strideLhs(dx: CGFloat, discontinuity: CGFloat) -> StrideTo<CGFloat> {
-        stride(from: startX + offset, to: discontinuity, by: dx)
+    
+    private func strideLhs(dx: CGFloat, to upperLimit: CGFloat) -> StrideTo<CGFloat> {
+        stride(from: startX + offset, to: upperLimit, by: dx)
     }
 
-    private func strideRhs(dx: CGFloat, discontinuityDelta: CGFloat) -> StrideTo<CGFloat> {
-        if let dc = discontinuity {
-            let postDc = dc + discontinuityDelta
-            return stride(from: postDc, to: endX, by: dx)
-        } else {
-            return stride(from: startX + offset, to: endX, by: dx)
-        }
+    private func strideAll(dx: CGFloat) -> StrideTo<CGFloat> {
+        stride(from: startX + offset, to: endX, by: dx)
+    }
+    
+    private func strideRhsOfDiscontinuity(dx: CGFloat, from: CGFloat, to upperLimit: CGFloat) -> StrideTo<CGFloat> {
+        stride(from: from, to: upperLimit, by: dx)
     }
 
     public var animatableData: AnimatablePair<CGFloat, CGFloat> {
@@ -114,12 +123,15 @@ public struct ChartLine: Shape {
 struct ChartLine_Previews: PreviewProvider {
 
     static var previews: some View {
-        ViewStateWrapper()
+        VStack {
+            ViewStateWrapper(t2: 0)
+            ViewStateWrapper(t2: 5)
+        }
     }
 
     struct ViewStateWrapper: View {
-        @State var t2: CGFloat = 0
         
+        let t2: CGFloat
         let discontinuity: CGFloat = 5
 
         var body: some View {
@@ -146,17 +158,17 @@ struct ChartLine_Previews: PreviewProvider {
                 .frame(width: 250, height: 250)
                 .border(Color.red)
 
-                Button(action: {
-                    withAnimation(.linear(duration: 1)) {
-                        if self.t2 == 50 {
-                            self.t2 = 0
-                        } else {
-                            self.t2 = 50
-                        }
-                    }
-                }) {
-                    Text("Click me")
-                }
+//                Button(action: {
+//                    withAnimation(.linear(duration: 1)) {
+//                        if self.t2 == 50 {
+//                            self.t2 = 0
+//                        } else {
+//                            self.t2 = 50
+//                        }
+//                    }
+//                }) {
+//                    Text("Click me")
+//                }
             }
         }
 
