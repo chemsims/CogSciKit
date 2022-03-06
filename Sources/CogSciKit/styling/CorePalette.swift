32393d8b6737eb31691d5f2143c769259791a329
Omar Fahmy <o.fahmy@outlook.com>
2022-03-06 14:25:59 +0400

    Moved CustomSlider to CogSciKit

--- Sources/CogSciKit/styling/CorePalette.swift
+++ Sources/CogSciKit/styling/CorePalette.swift
@@ -10,5 +10,15 @@ public struct CorePalette {
     public static let orangeAccent = Color("orangeAccent", bundle: .module)
     public static let speechBubble = Color("speechBubble", bundle: .module)
     
+    
+    
+    
     static let inactiveSpeechBubbleButton = Color("inactiveSpeechBubble", bundle: .module)
+    
+    
+    public struct Slider {
+        public static let bar = Color("sliderBar", bundle: .module)
+        public static let enabledHandle = CorePalette.orangeAccent
+        public static let disabledHandle = Color("disabledSliderHandle", bundle: .module)
+    }
 }
---
+++ Sources/CogSciKit/view/slider/DisabledSliderModifier.swift
@@ -0,0 +1,21 @@
+//
+// CogSciKit
+//
+
+import SwiftUI
+
+public struct DisabledSliderModifier: ViewModifier {
+    private let disabled: Bool
+
+    public init(disabled: Bool) {
+        self.disabled = disabled
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .disabled(disabled)
+            .compositingGroup()
+            .colorMultiply(disabled ? .gray : .white)
+            .opacity(disabled ? 0.3 : 1)
+    }
+}
