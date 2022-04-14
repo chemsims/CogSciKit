4e68fb1e0fddffe50403dc680142e833d82a8e11
Omar Fahmy <o.fahmy@outlook.com>
2022-04-14 08:16:58 +0100

    Support creating slider haptics handler without an axis instance

--- Sources/CogSciKit/model/slider/SliderHapticsHandler.swift
+++ Sources/CogSciKit/model/slider/SliderHapticsHandler.swift
@@ -8,12 +8,51 @@ private let defaultPreparationBufferPercentage: CGFloat = 0.1
 
 public struct SliderHapticsHandler<Value: BinaryFloatingPoint> {
     
-    let axis: LinearAxis<Value>
+    let minValue: Value
+    let maxValue: Value
     let impactGenerator:UIImpactFeedbackGenerator
     private let upperValueToPrepareGenerator: Double
     private let lowerValueToPrepareGenerator: Double
 
-    /// Creates a haptics handler which may prepare or fires the underlying `impactGenerator` when
+    internal init(
+        minValue: Value,
+        maxValue: Value,
+        impactGenerator: UIImpactFeedbackGenerator,
+        preparationBufferPercentage: Double
+    ) {
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.impactGenerator = impactGenerator
+        let changeInValue = maxValue - minValue
+        let absolutePreparationBuffer = preparationBufferPercentage * Double(changeInValue)
+        self.upperValueToPrepareGenerator = Double(maxValue) - absolutePreparationBuffer
+        self.lowerValueToPrepareGenerator = Double(minValue) + absolutePreparationBuffer
+    }
+
+    /// Triggers a haptic impact when the `newValue` exceeds the axis limits, and
+    /// prepares the generator when value is close to the limits.
+    public func valueDidChange(
+        newValue: Value,
+        oldValue: Value
+    ) {
+        if newValue > oldValue {
+            if newValue >= maxValue {
+                impactGenerator.impactOccurred()
+            } else if Double(newValue) >= upperValueToPrepareGenerator {
+                impactGenerator.prepare()
+            }
+        } else if newValue < oldValue {
+            if newValue <= minValue {
+                impactGenerator.impactOccurred()
+            } else if Double(newValue) <= lowerValueToPrepareGenerator {
+                impactGenerator.prepare()
+            }
+        }
+    }
+}
+
+extension SliderHapticsHandler {
+    /// Creates a haptics handler which may prepare or fire the underlying `impactGenerator` when
     /// `valueDidChange` is called.
     ///
     /// - Parameters:
@@ -30,37 +69,35 @@ public struct SliderHapticsHandler<Value: BinaryFloatingPoint> {
         )
     }
     
-    init(
+    /// Creates a haptics handler which may prepare or fire the underlying `impactGenerator` when
+    /// `valueDidChange` is called.
+    ///
+    /// - Parameters:
+    ///   - axis: The axis of the slider.
+    ///   - impactGenerator: The haptic feedback generator.
+    public init(
+        minValue: Value,
+        maxValue: Value,
+        impactGenerator: UIImpactFeedbackGenerator
+    ) {
+        self.init(
+            minValue: minValue,
+            maxValue: maxValue,
+            impactGenerator: impactGenerator,
+            preparationBufferPercentage: defaultPreparationBufferPercentage
+        )
+    }
+    
+    internal init(
         axis: LinearAxis<Value>,
         impactGenerator: UIImpactFeedbackGenerator,
         preparationBufferPercentage: Double
     ) {
-        self.axis = axis
-        self.impactGenerator = impactGenerator
-        let changeInValue = axis.maxValue - axis.minValue
-        let absolutePreparationBuffer = preparationBufferPercentage * Double(changeInValue)
-        self.upperValueToPrepareGenerator = Double(axis.maxValue) - absolutePreparationBuffer
-        self.lowerValueToPrepareGenerator = Double(axis.minValue) + absolutePreparationBuffer
-    }
-
-    /// Triggers a haptic impact when the `newValue` exceeds the axis limits, and
-    /// prepares the generator when value is close to the limits.
-    public func valueDidChange(
-        newValue: Value,
-        oldValue: Value
-    ) {
-        if newValue > oldValue {
-            if newValue >= axis.maxValue {
-                impactGenerator.impactOccurred()
-            } else if Double(newValue) >= upperValueToPrepareGenerator {
-                impactGenerator.prepare()
-            }
-        } else if newValue < oldValue {
-            if newValue <= axis.minValue {
-                impactGenerator.impactOccurred()
-            } else if Double(newValue) <= lowerValueToPrepareGenerator {
-                impactGenerator.prepare()
-            }
-        }
+        self.init(
+            minValue: axis.minValue,
+            maxValue: axis.maxValue,
+            impactGenerator: impactGenerator,
+            preparationBufferPercentage: preparationBufferPercentage
+        )
     }
 }
